865575772ffc6fbd48dfa3e1df111075d81a7169
Michael Scherer <mail@scherermichael.net>
2018-08-03 21:18:18 +0200

    Add some tests.

---
+++ test/unit/lib/extractConventionalMessage.test.js
@@ -0,0 +1,82 @@
+const extractConventionalCommitMessage = require('../../../lib/extractConventionalCommitMessage')
+
+describe('extractConventionalCommitMessage', () => {
+  it('is a function.', async () => {
+    expect(extractConventionalCommitMessage).toBeInstanceOf(Function)
+  })
+
+  it('extracts the message from the end of the body.', async () => {
+    const body = [
+      'line 1',
+      'line 2',
+      '## Release Notes',
+      'message 1',
+      'message 2'
+    ].join('\n')
+    const expected = [
+      'message 1',
+      'message 2'
+    ].join('\n')
+
+    expect(extractConventionalCommitMessage(body)).toBe(expected)
+  })
+
+  it('extracts the message from the start of the body.', async () => {
+    const body = [
+      '## Release Notes',
+      'message 1',
+      'message 2',
+      '## Other Section',
+      'line 1',
+      'line 2'
+    ].join('\n')
+    const expected = [
+      'message 1',
+      'message 2'
+    ].join('\n')
+
+    expect(extractConventionalCommitMessage(body)).toBe(expected)
+  })
+
+  it('extracts the message from the middle of the body.', async () => {
+    const body = [
+      '## First Section',
+      'line 1',
+      'line 2',
+      '## Release Notes',
+      'message 1',
+      'message 2',
+      '## Other Section',
+      'line 1',
+      'line 2'
+    ].join('\n')
+    const expected = [
+      'message 1',
+      'message 2'
+    ].join('\n')
+
+    expect(extractConventionalCommitMessage(body)).toBe(expected)
+  })
+
+  it('trims the message.', async () => {
+    const body = [
+      '## First Section',
+      'line 1',
+      'line 2',
+      '## Release Notes',
+      '   ',
+      'message 1',
+      'message 2',
+      '',
+      '## Other Section',
+      'line 1',
+      'line 2'
+    ].join('\n')
+    const expected = [
+      'message 1',
+      'message 2'
+    ].join('\n')
+
+    expect(extractConventionalCommitMessage(body)).toBe(expected)
+  })
+})
---
+++ test/unit/lib/getReleaseType.test.js
@@ -0,0 +1,33 @@
+const getReleaseType = require('../../../lib/getReleaseType')
+
+describe('getReleaseType', () => {
+  it('is a function.', async () => {
+    expect(getReleaseType).toBeInstanceOf(Function)
+  })
+
+  it('detects major release.', async () => {
+    const message = [
+      'feat: message 1',
+      '',
+      'Breaking Change: message 2'
+    ].join('\n')
+
+    expect(await getReleaseType(message)).toEqual('major')
+  })
+
+  it('interpetes \'feat\' as minor release.', async () => {
+    const message = [
+      'feat: message 1'
+    ].join('\n')
+
+    expect(await getReleaseType(message)).toEqual('minor')
+  })
+
+  it('interpetes \'fix\' as patch release.', async () => {
+    const message = [
+      'fix: message 1'
+    ].join('\n')
+
+    expect(await getReleaseType(message)).toEqual('patch')
+  })
+})
